38998c389bd1bc22637d11707d41b8be0d1ae4a2
Paul Calinovici <calinovicipaul@gmail.com>
2017-07-14 17:43:52 +0300

    spring security part1

---
+++ spring-tutorial-91/src/com/caveofprogramming/spring/web/controllers/LoginController.java
@@ -0,0 +1,64 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import com.caveofprogramming.spring.web.dao.User;
+import com.caveofprogramming.spring.web.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+/**
+ * Created by IrianLaptop on 7/14/2017.
+ */
+
+@Controller
+public class LoginController {
+
+
+    @Autowired
+    private UsersService usersService;
+
+    @RequestMapping("/login")
+    public String showLogin() {
+        return "login";
+    }
+
+
+    @RequestMapping("/newaccount")
+    public String showNewAccount(Model model) {
+
+        model.addAttribute("user", new User());
+        return "newaccount";
+    }
+
+
+    @RequestMapping(value = "/createaccount", method = RequestMethod.POST)
+    public String createAccount(@Valid User user, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "newaccount";
+        }
+
+        user.setAuthority("user");
+        user.setEnabled(true);
+
+        if(usersService.exists(user.getUsername())){
+            result.rejectValue("username", "DuplicateKey.user.username" );
+            return "newaccount";
+        }
+        try {
+            usersService.create(user);
+        }catch (DuplicateKeyException ex){
+            result.rejectValue("username", "DuplicateKey.user.username");
+            return "newaccount";
+        }
+
+        return "accountcreated";
+    }
+}
